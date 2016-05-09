c5c5beb1490fd4a03e8e79ce3674001864897759
SiphiweN <siphiwenyathi3@gmail.com>
2016-05-09 12:33:20 +0200

    Added initial application

---
+++ PhotoSharingApplication/Scripts/ChatRoom.js
@@ -0,0 +1,32 @@
+﻿
+
+$(function ()
+{
+    var chat = $.connection.chatHub;
+
+    chat.client.addMessage = function (name, message)
+    {
+        var encodedName = $('<div />').text(name).html();
+
+        var encodedMessage = $('<div />').text(message).html();
+
+        var listItem = '<li>' + encodedName + ' : ' + encodedMessage + '</li>';
+
+        $('#discussion').append(listItem);
+
+    };
+
+    $('#chat-message').focus();
+
+    $.connection.hub.start().done(function ()
+    {
+        chat.server.join(photoid).done(function ()
+        {
+            $('#sendmessage').click(function () {
+                chat.server.send(username, photoid, $('#chat-message').val());
+
+                $('#chat-message').val(' ').focus();
+            });
+        });
+    });
+});
